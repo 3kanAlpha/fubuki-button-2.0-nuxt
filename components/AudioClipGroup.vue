d91906820c2ec953f115597f93cb78331af07a11
3kanAlpha <39447635+3kanAlpha@users.noreply.github.com>
2023-09-29 15:15:03 +0900

    style: aboutページのデザイン変更

--- components/AudioClipGroup.vue
+++ components/AudioClipGroup.vue
@@ -1,13 +1,13 @@
 <template>
   <q-card class="card-area q-ma-md">
-      <q-card-section>
-        <div class="text-h6">{{ props.group.group_description['ja'] }}</div>
-      </q-card-section>
+    <q-card-section>
+      <div class="text-h6">{{ props.group.group_description['ja'] }}</div>
+    </q-card-section>
 
-      <q-card-section>
-        <AudioClipButton v-for="voice in props.group.voice_list" :label="voice.description.ja" :audio-path="voice.path" />
-      </q-card-section>
-    </q-card>
+    <q-card-section>
+      <AudioClipButton v-for="voice in props.group.voice_list" :label="voice.description.ja" :audio-path="voice.path" />
+    </q-card-section>
+  </q-card>
 </template>
 
 <script setup>
--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,15 @@
 <template>
   <NuxtLayout>
-    工事中だよ
+    <q-layout style="min-height: 500px">
+      <q-card class="card-area q-ma-md about-md">
+        <q-card-section>
+          工事中だよ
+        </q-card-section>
+      </q-card>
+    </q-layout>
   </NuxtLayout>
 </template>
+
+<style lang="scss" scoped>
+@use "~/assets/markdown.css";
+</style>
